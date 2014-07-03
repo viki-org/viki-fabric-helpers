02fa415756bf19977668c036064bffdfd333cd05
Pang Yan Han <yanhan@viki.com>
2014-07-03 12:12:08 +0800

    viki.fabric.git - add `_` prefix to private globals
    Signed-off-by: Pang Yan Han <yanhan@viki.com>

--- viki/fabric/git.py
+++ viki/fabric/git.py
@@ -12,24 +12,24 @@ from viki.fabric import VIKI_FABRIC_CONFIG_KEY_NAME
 from viki.fabric.helpers import env_has_nested_keys, get_in_viki_fabric_config
 
 # Whether the `_initialize` function has been called
-INITIALIZED = False
+_INITIALIZED = False
 
 # Name of the ssh private key that allows us to clone private analytics Github
 # repositories
-SSH_PRIVATE_KEY = None
+_SSH_PRIVATE_KEY = None
 
 # Public key for the above private key
-SSH_PUBLIC_KEY = None
+_SSH_PUBLIC_KEY = None
 
-# Directory name on the server (under $HOME) where the `SSH_PRIVATE_KEY` and
-# `SSH_PUBLIC_KEY` files are stored
-SSH_KEYS_DIR = None
+# Directory name on the server (under $HOME) where the `_SSH_PRIVATE_KEY` and
+# `_SSH_PUBLIC_KEY` files are stored
+_SSH_KEYS_DIR = None
 
-# Local directory where the `SSH_PRIVATE_KEY` and `SSH_PUBLIC_KEY` are stored
-SSH_KEYS_LOCAL_COPY_DIR = None
+# Local directory where the `_SSH_PRIVATE_KEY` and `_SSH_PUBLIC_KEY` are stored
+_SSH_KEYS_LOCAL_COPY_DIR = None
 
-# Folder storing template for `GIT_SSH_SCRIPT_NAME`
-GIT_SSH_SCRIPT_LOCAL_FOLDER = None
+# Folder storing template for `_GIT_SSH_SCRIPT_NAME`
+_GIT_SSH_SCRIPT_LOCAL_FOLDER = None
 
 # Script to use like this:
 #
@@ -40,16 +40,16 @@ GIT_SSH_SCRIPT_LOCAL_FOLDER = None
 #     GIT_SSH=$HOME/gitwrap.sh git fetch origin
 #
 # This is for cloning from secret repositories, and makes use of the
-# `SSH_PRIVATE_KEY`
-GIT_SSH_SCRIPT_NAME = None
+# `_SSH_PRIVATE_KEY`
+_GIT_SSH_SCRIPT_NAME = None
 
 def _initialize():
   """Initializes some global variables in this module with those read from the
   `viki_fabric_config.yml` file (and subsequently stored into
   `env.viki_fabric_config["viki.fabric.git"]`).
   """
-  global INITIALIZED
-  if INITIALIZED:
+  global _INITIALIZED
+  if _INITIALIZED:
     return
 
   if not env_has_nested_keys([VIKI_FABRIC_CONFIG_KEY_NAME, "viki.fabric.git"]):
@@ -75,17 +75,17 @@ def _initialize():
     assert(k in vikiFabricGitConfig)
 
   # assign them to global variables for convenience
-  global SSH_PRIVATE_KEY, SSH_PUBLIC_KEY, SSH_KEYS_LOCAL_COPY_DIR, \
-         SSH_KEYS_DIR, GIT_SSH_SCRIPT_LOCAL_FOLDER, GIT_SSH_SCRIPT_NAME
-  SSH_PRIVATE_KEY = vikiFabricGitConfig["ssh_private_key"]
-  SSH_PUBLIC_KEY = vikiFabricGitConfig["ssh_public_key"]
-  SSH_KEYS_LOCAL_COPY_DIR = vikiFabricGitConfig["ssh_keys_local_copy_dir"]
-  SSH_KEYS_DIR = vikiFabricGitConfig["ssh_keys_dir"]
-  GIT_SSH_SCRIPT_NAME = vikiFabricGitConfig["git_ssh_script_name"]
-  GIT_SSH_SCRIPT_LOCAL_FOLDER = \
+  global _SSH_PRIVATE_KEY, _SSH_PUBLIC_KEY, _SSH_KEYS_LOCAL_COPY_DIR, \
+         _SSH_KEYS_DIR, _GIT_SSH_SCRIPT_LOCAL_FOLDER, _GIT_SSH_SCRIPT_NAME
+  _SSH_PRIVATE_KEY = vikiFabricGitConfig["ssh_private_key"]
+  _SSH_PUBLIC_KEY = vikiFabricGitConfig["ssh_public_key"]
+  _SSH_KEYS_LOCAL_COPY_DIR = vikiFabricGitConfig["ssh_keys_local_copy_dir"]
+  _SSH_KEYS_DIR = vikiFabricGitConfig["ssh_keys_dir"]
+  _GIT_SSH_SCRIPT_NAME = vikiFabricGitConfig["git_ssh_script_name"]
+  _GIT_SSH_SCRIPT_LOCAL_FOLDER = \
     vikiFabricGitConfig["git_ssh_script_local_folder"]
 
-  INITIALIZED = True
+  _INITIALIZED = True
 
 def _check_initialized(f):
   """A decorator which checks that the `_initialize` function has been called;
@@ -150,10 +150,10 @@ def setup_server_for_git_clone(homeDir=None):
   if homeDir is None:
     homeDir = fabric_helpers.get_home_dir()
   serverName = env.host
-  serverPublicKeyPath = os.path.join(homeDir, SSH_KEYS_DIR, SSH_PUBLIC_KEY)
-  serverPrivateKeyPath = os.path.join(homeDir, SSH_KEYS_DIR, SSH_PRIVATE_KEY)
-  localPublicKey = os.path.join(SSH_KEYS_LOCAL_COPY_DIR, SSH_PUBLIC_KEY)
-  localPrivateKey = os.path.join(SSH_KEYS_LOCAL_COPY_DIR, SSH_PRIVATE_KEY)
+  serverPublicKeyPath = os.path.join(homeDir, _SSH_KEYS_DIR, _SSH_PUBLIC_KEY)
+  serverPrivateKeyPath = os.path.join(homeDir, _SSH_KEYS_DIR, _SSH_PRIVATE_KEY)
+  localPublicKey = os.path.join(_SSH_KEYS_LOCAL_COPY_DIR, _SSH_PUBLIC_KEY)
+  localPrivateKey = os.path.join(_SSH_KEYS_LOCAL_COPY_DIR, _SSH_PRIVATE_KEY)
   fabric_helpers.copy_file_to_server_if_not_exists(localPublicKey,
     serverPublicKeyPath
   )
@@ -164,8 +164,8 @@ def setup_server_for_git_clone(homeDir=None):
   # the ssh private key we just copied
   gitSSHWrapperPath = get_git_ssh_script_path(homeDir)
   if not exists(gitSSHWrapperPath):
-    upload_template(GIT_SSH_SCRIPT_NAME, gitSSHWrapperPath, use_jinja=True, 
-      template_dir=GIT_SSH_SCRIPT_LOCAL_FOLDER, mode=0755,
+    upload_template(_GIT_SSH_SCRIPT_NAME, gitSSHWrapperPath, use_jinja=True,
+      template_dir=_GIT_SSH_SCRIPT_LOCAL_FOLDER, mode=0755,
       context={ 'ssh_private_key_path': serverPrivateKeyPath }
     )
 
@@ -238,7 +238,7 @@ def get_git_ssh_script_path(homeDir=None):
   """
   if homeDir is None:
     homeDir = fabric_helpers.get_home_dir()
-  return os.path.join(homeDir, GIT_SSH_SCRIPT_NAME)
+  return os.path.join(homeDir, _GIT_SSH_SCRIPT_NAME)
 
 @_check_initialized
 def _get_ssh_public_key_path(homeDir=None):
@@ -254,7 +254,7 @@ def _get_ssh_public_key_path(homeDir=None):
   """
   if homeDir is None:
     homeDir = fabric_helpers.get_home_dir()
-  return os.path.join(homeDir, SSH_KEYS_DIR, SSH_PUBLIC_KEY)
+  return os.path.join(homeDir, _SSH_KEYS_DIR, _SSH_PUBLIC_KEY)
 
 @_check_initialized
 def _get_ssh_private_key_path(homeDir=None):
@@ -270,4 +270,4 @@ def _get_ssh_private_key_path(homeDir=None):
   """
   if homeDir is None:
     homeDir = fabric_helpers.get_home_dir()
-  return os.path.join(homeDir, SSH_KEYS_DIR, SSH_PRIVATE_KEY)
+  return os.path.join(homeDir, _SSH_KEYS_DIR, _SSH_PRIVATE_KEY)
